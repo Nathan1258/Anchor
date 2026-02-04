c3f96ed42cb619c5cab5b2aa4e5d932553bd6d2f
Nathan Ellis <nathan@ellisn.com>
2026-02-04 17:32:24 +0000

    Initial Commit

---
+++ Anchor/AnchorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnchorApp.swift
+//  Anchor
+//
+//  Created by Nathan Ellis on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct AnchorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
